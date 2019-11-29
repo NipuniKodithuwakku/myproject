e0b49ebc9232f6cf4a5d907dee6ec2b7a39cbb6c
NipuniKodithuwakku <chandulakan123@gmail.com>
2019-11-28 19:11:49 -0800

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+
+class Home extends StatefulWidget{
+  @override
+  State<StatefulWidget>createState(){
+    return new HomeState();
+  }
+}
+
+class HomeState extends State<Home>{
+  @override
+  int radioValue=0;
+  double _FinalResult=0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  } 
+}
